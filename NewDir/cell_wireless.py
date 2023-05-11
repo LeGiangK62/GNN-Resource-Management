2960076cecf17f68e217b9423762498fa47b64c3
LeGiangK62 <tung.giangle99@gmail.com>
2023-05-11 10:34:34 +0900

    Fix cell_wireless

--- NewDir/cell_wireless.py
+++ NewDir/cell_wireless.py
@@ -5,7 +5,7 @@ from scipy.spatial import distance_matrix
 from torch_geometric.loader import DataLoader
 from torch_geometric.data import Data
 
-from NewDir.reImplement import GCNet
+from reImplement import GCNet
 
 
 def generate_channels_cell_wireless(num_bs, num_users, num_samples, var_noise=1.0, radius=1):
@@ -85,31 +85,31 @@ def adj_matrix(num_users):
 
 # GNN configuration
 
-def loss_function(data, out, device_type,):
+def loss_function(out, regularization, p_max):
     power = out[:, 2]
-    disired_channels = out[:,0]
-
     num_user = out.shape[0]
-    noise_var = out[1,1]
+    noise_var = out[1, 1]
     power = torch.reshape(power, (-1, num_user, 1))
-
-    abs_H = data.y
+    abs_H = out[:, 0]
     abs_H_2 = torch.pow(abs_H, 2)
-    rx_power = torch.mul(abs_H_2, power)
-    mask = torch.eye(num_user)
-    mask = mask.to(device_type)
-    valid_rx_power = torch.sum(torch.mul(rx_power, mask), 1)
-    interference = torch.sum(torch.mul(rx_power, 1 - mask), 1) + noise_var
-    rate = torch.log(1 + torch.div(valid_rx_power, interference))
-    sum_rate = torch.mean(torch.sum(rate, 1))
+    all_signal = torch.mul(abs_H_2, power)[0]
+    ############
+    desired_sig = torch.diag(all_signal)
 
-    p_constraint = torch.sum(1)
+    noise = torch.ones((1, num_user)) * noise_var
+    interference = all_signal
+    interference.fill_diagonal_(0)
+    interference = torch.sum(interference, 0)
+    rate = torch.log(1 + torch.div(desired_sig, interference + noise))
+    p_constraint = power - torch.ones((1, num_user)) * p_max
+    ################
+    sum_rate = torch.mean(torch.sum(rate, 1) - regularization * p_constraint)
 
-    loss = torch.neg(sum_rate) + p_constraint
+    loss = torch.neg(sum_rate)
     return loss
 
 
-def model_training(num_user, noise_var, model, train_load, device_type, num_samples, optimizer):
+def model_training(regularization, p_max, model, train_load, device_type, num_samples, optimizer):
     model.train()
 
     total_loss = 0
@@ -117,41 +117,37 @@ def model_training(num_user, noise_var, model, train_load, device_type, num_samp
         data = data.to(device_type)
         optimizer.zero_grad()
         out = model(data)
-        loss = loss_function(data, out, num_user, device_type, noise_var)
+        loss = loss_function(out, regularization, p_max)
         loss.backward()
         total_loss += loss.item() * data.num_graphs
         optimizer.step()
     return total_loss / num_samples
 
 
-def model_testing(num_user, noise_var, model, test_load, device_type, num_test):
+def model_testing(regularization, p_max, model, test_load, device_type, num_test):
     model.eval()
-
     total_loss = 0
     for data in test_load:
         data = data.to(device_type)
         with torch.no_grad():
             out = model(data)
-            loss = loss_function(data, out, num_user, device_type, noise_var)
+            loss = loss_function(out, regularization, p_max)
             total_loss += loss.item() * data.num_graphs
     return total_loss / num_test
 
 
-def graph_build(channel_matrix, adjacency_matrix):
+def graph_build(channel_matrix, adjacency_matrix, noise_var):
     num_user = channel_matrix.shape[1]
-    x = np.concatenate((
-        np.transpose(channel_matrix),
-        var_train,
-        np.ones((num_user, 1))
-    ),
-        axis=1
-    )
+    x1 = np.transpose(channel_matrix)
+    x2 = np.ones((num_user, 1)) * noise_var
+    x3 = np.ones((num_user, 1))
+    x = np.concatenate((x1, x2, x3),axis=1)
     edge_index = adjacency_matrix
     edge_attr = []
     for each_interfence in adjacency_matrix:
         tx = each_interfence[0]
         rx = each_interfence[1]
-        tmp = [channel_matrix[tx, rx], channel_matrix[rx, tx]]
+        tmp = [channel_matrix[0, tx], channel_matrix[0, rx]]
         edge_attr.append(tmp)
     # y =
     # pos =
@@ -164,14 +160,15 @@ def graph_build(channel_matrix, adjacency_matrix):
     return data
 
 
-def process_data(channel_matrices):
+def process_data(channel_matrices, noise_var):
     num_samples = channel_matrices.shape[0]
-    num_user = channel_matrices.shape[1]
+    num_user = channel_matrices.shape[2]
     data_list = []
     adj = adj_matrix(num_user)
     for i in range(num_samples):
         data = graph_build(channel_matrix=channel_matrices[i],
-                           adjacency_matrix=adj
+                           adjacency_matrix=adj,
+                           noise_var=noise_var
                            )
         data_list.append(data)
     return data_list
@@ -179,50 +176,42 @@ def process_data(channel_matrices):
 
 if __name__ == '__main__':
 
-    a = np.array([[1, 2, 3, 4, 5, 6, 7, 8, 9]])
-    b = a
-    a = torch.from_numpy(a)
-    b = torch.from_numpy(b)
-    bt = torch.t(b)
-    print(bt)
-    print(b)
-    print(torch.mul(a,b).shape)
-    print(torch.mul(a,bt).shape)
-    # K = 1   # number of BS(s)
-    # N = 10  # number of users
-    # R = 10  # radius
+    K = 1  # number of BS(s)
+    N = 10  # number of users
+    R = 10  # radius
+
+    num_train = 100  # number of training samples
+    num_test = 10  # number of test samples
+
+    reg = 1e-2
+    pmax = 1
+    var_db = 10
+    var = 1 / 10 ** (var_db / 10)
+
+    X_train, pos_train, adj_train = generate_channels_cell_wireless(K, N, num_train, var, R)
+    X_test, pos_test, adj_test = generate_channels_cell_wireless(K, N, num_test, var, R)
+    # print(channel_matrices.shape)
+    # print(positions.shape)
+    # print(adj_matrix.shape)
     #
-    # num_train = 100  # number of training samples
-    # num_test = 10   # number of test samples
-    #
-    # var_db = 10
-    # var = 1 / 10 ** (var_db / 10)
-    #
-    # var_train = np.ones((num_train,1)) * var
-    # X_train, pos_train, adj_train = generate_channels_cell_wireless(K, N, num_train, var, R)
-    # X_test, pos_test, adj_test = generate_channels_cell_wireless(K, N, num_test, var, R)
-    # # print(channel_matrices.shape)
-    # # print(positions.shape)
-    # # print(adj_matrix.shape)
-    # #
-    # # gcn_model = GCNet()
-    #
-    # train_data = process_data(X_train)
-    # test_data = process_data(X_test)
-    # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    # print(torch.cuda.is_available())
-    # gcn_model = GCNet().to(device)
-    # optimizer = torch.optim.Adam(gcn_model.parameters(), lr=0.001)
-    # scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.9)
-    # train_loader = DataLoader(train_data, batch_size=64, shuffle=True, num_workers=1)
-    # test_loader = DataLoader(test_data, batch_size=2000, shuffle=False, num_workers=1)
-    #
-    # for epoch in range(1, 200):
-    #     loss1 = model_training(N, var, gcn_model, train_loader, device, num_train, optimizer)
-    #     if epoch % 8 == 0:
-    #         loss2 = model_testing(N, var, gcn_model, test_loader, device, num_train)
-    #         print('Epoch {:03d}, Train Loss: {:.4f}, Val Loss: {:.4f}'.format(
-    #             epoch, loss1, loss2))
-    #     scheduler.step()
-    #
-    # torch.save(gcn_model.state_dict(), 'model.pth')
+    # gcn_model = GCNet()
+
+    train_data = process_data(X_train, var)
+    test_data = process_data(X_test, var)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(torch.cuda.is_available())
+    gcn_model = GCNet().to(device)
+    optimizer = torch.optim.Adam(gcn_model.parameters(), lr=0.001)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.9)
+    train_loader = DataLoader(train_data, batch_size=64, shuffle=True, num_workers=1)
+    test_loader = DataLoader(test_data, batch_size=2000, shuffle=False, num_workers=1)
+
+    for epoch in range(1, 200):
+        loss1 = model_training(reg, pmax, gcn_model, train_loader, device, num_train, optimizer)
+        if epoch % 8 == 0:
+            loss2 = model_testing(N, var, gcn_model, test_loader, device, num_train)
+            print('Epoch {:03d}, Train Loss: {:.4f}, Val Loss: {:.4f}'.format(
+                epoch, loss1, loss2))
+        scheduler.step()
+
+    torch.save(gcn_model.state_dict(), 'model.pth')
